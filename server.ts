5363894f5bd2d52632a1531e5129cfacee6672d1
MuhireIghor <muhireighor123@gmail.com>
2024-05-15 15:47:52 +0200

    added the initial proj setup

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import cors from "cors"
+import { config } from "dotenv";
+import { ENDPOINT } from "./utils/core.util";
+import authRoute from "./routes/user.route";
+config();
+const port = process.env.PORT || 8000;
+const app = express();
+app.use(express.json())
+app.use(cors({
+    origin: "*"
+}))
+app.use(`${ENDPOINT}/users`,authRoute)
+app.get("/", async (req: Request, res: Response) => {
+    return res.status(200).json({
+        message: "Welcome to supa menu apis"
+    })
+})
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
