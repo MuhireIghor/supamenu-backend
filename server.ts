e3701b5c4d45cb48fa021e2135eadee4f26f2d01
MuhireIghor <muhireighor123@gmail.com>
2024-05-17 11:52:53 +0200

    added the restoapi routes

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import { config } from "dotenv";
 import { ENDPOINT } from "./utils/core.util";
 import authRoute from "./routes/user.route";
+import restoRoute from "./routes/restaurant.route";
 config();
 const port = process.env.PORT || 8000;
 const app = express();
@@ -10,7 +11,8 @@ app.use(express.json())
 app.use(cors({
     origin: "*"
 }))
-app.use(`${ENDPOINT}/users`,authRoute)
+app.use(`${ENDPOINT}/users`, authRoute)
+app.use(`${ENDPOINT}/restaurants`, restoRoute)
 app.get("/", async (req: Request, res: Response) => {
     return res.status(200).json({
         message: "Welcome to supa menu apis"
